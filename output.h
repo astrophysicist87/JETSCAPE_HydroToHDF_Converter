93190a0598dddeb22fe413da6cc7b857af08a21d
Christopher Plumberg <plumberg.1@osu.edu>
2021-02-02 13:07:13 -0600

    Update output.h

--- output.h
+++ output.h
@@ -81,10 +81,12 @@ void output_to_HDF( const vector<vector<double> > & v, string outfilename )
 }
 
 
-void output_to_HDF_for_JETSCAPE( const vector<vector<double> > & v, string outfilename )
+void output_to_HDF_for_JETSCAPE( const vector<vector<double> > & v, string outfilename/*,
+		double DX, double DY, int OutputViscousFlag, double Tau0, double TauFS,
+		int XH, int XL, int YH, int YL, double dTau*/ )
 {
 	const H5std_string	FILE_NAME(outfilename.c_str());
-	const H5std_string	DATASET_NAME("hydro");
+	//const H5std_string	DATASET_NAME("hydro");
 	const int	 NX = v.size();
 	const int	 NY = (v[0]).size();
 	const int	 RANK = 2;
@@ -99,16 +101,10 @@ void output_to_HDF_for_JETSCAPE( const vector<vector<double> > & v, string outfi
 		Exception::dontPrint();
 	
 		H5File file(FILE_NAME, H5F_ACC_TRUNC);
-		//H5File* file = new H5File(FILE_NAME, H5F_ACC_TRUNC);
-	
-    	//Group group = file.createGroup( "/" );
-    	//Group eventGroup = file.createGroup( "/EVENT" );
-    	//Group* group = new Group( file->createGroup( "/" ) );
-    	//Group* eventGroup = new Group( file.createGroup( "/EVENT" ) );
 
 		Group groupEvent(file.createGroup("/Event"));
 
-		double DX = 0.1;
+		//double DX = 0.1;
 		const int RANK = 1;
 		hsize_t dims[RANK];
 		dims[0] = 1;
@@ -116,22 +112,19 @@ void output_to_HDF_for_JETSCAPE( const vector<vector<double> > & v, string outfi
 		Attribute att = groupEvent.createAttribute("DX", PredType::NATIVE_DOUBLE, dspace );
 		att.write(PredType::NATIVE_DOUBLE, &DX);
 
-        //hid_t space_id = H5Screate_simple(rank, current_dims, max_dims);
 
-		/*ATTRIBUTE "DX" {
-         DATATYPE  H5T_IEEE_F64LE
-         DATASPACE  SIMPLE { ( 1 ) / ( 1 ) }
-         DATA {
-         (0): 0.288
-         }*/
-		/*hsize_t dims[2];
-		dims[0] = NX;
-		dims[1] = NY;
-		DataSpace dataspace(RANK, dims);
-	
-		DataSet dataset = file.createDataSet(DATASET_NAME, PredType::NATIVE_DOUBLE, dataspace);
-			
-		dataset.write(data, PredType::NATIVE_DOUBLE);*/	
+		{	
+			Group groupFrame(file.createGroup("/Event/Frame_0000"));
+			hsize_t loc_dims[2];
+			loc_dims[0] = NX;
+			loc_dims[1] = NY;
+			DataSpace dataspace(2, loc_dims);
+		
+			DataSet dataset = groupFrame.createDataSet("/Event/Frame_0000/hydro",
+														PredType::NATIVE_DOUBLE, dataspace);
+				
+			dataset.write(data, PredType::NATIVE_DOUBLE);
+		}
     }
 
     catch(FileIException error)
